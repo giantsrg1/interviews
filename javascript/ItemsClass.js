513cff2a4407a1710235c523279de3c37efa60ac
Scott Goldstein <goldstein.scott@aol.com>
2017-11-20 21:45:20 -0500

    Add comments to ItemsClass.js

--- javascript/ItemsClass.js
+++ javascript/ItemsClass.js
@@ -1,4 +1,7 @@
+//main package class
 class packages {
+
+    //set up our options for the packages
     constructor() {
         this.packageOptions = {
             whiteLabel: {
@@ -12,10 +15,12 @@ class packages {
         };
     }
 
+    //set the option user wants
     setPackageOption(opt){
     	this.userPackageOpt = opt;
     }
 
+    //get the option price (opt: ask for price for certain item) or the user set option
     getOptionsPrice(opt) {
         if(opt){
     		return this.packageOptions[opt].p;
@@ -25,6 +30,7 @@ class packages {
 	    }
     }
 
+    //get the option name (opt: ask for name for certain item) or the user set option
     getOptionsName(opt) {
     	if(opt){
     		return this.packageOptions[opt].name;
@@ -34,6 +40,7 @@ class packages {
 	    }
     }
 
+    //get options list for view
     getOptions() {
         return this.packageOptions;
     }
@@ -41,15 +48,18 @@ class packages {
 
 
 class cartItems extends packages {
+    //create cartItems with old items call super class
     constructor(oldCart) {
     	super();
         this.items = oldCart || [];
     }
 
+    //add item to cart
     add(item) {
         this.items.push(item);
     }
 
+    //get the sum and if user picks special package add to sum
     getSum() {
         var sum = 0;
         this.items.forEach(function(item) {
@@ -61,21 +71,28 @@ class cartItems extends packages {
         return sum;
     }
 
+    //get items in cart
     getItems() {
         return this.items;
     }
 }
 
+//create cart
 var cart = new cartItems();
 
+//add items to cart
 cart.add({ p: 1.28, q: 2 });
 cart.add({ p: 3.99, q: 17 });
 cart.add({ p: 4.99, q: 2 });
 
+//Get sum of just the cart
 console.log('Sum:' + cart.getSum());
 
+//select package (gift or whitelabel)
 cart.setPackageOption('gift');
 
+//get the option and price
 console.log('Package ' + cart.getOptionsName() + ' was chosen! for $'+ cart.getOptionsPrice());
 
+// get new sum of the cart after selection
 console.log('New Sum:' + cart.getSum());
